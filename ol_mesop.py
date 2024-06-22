866e4bd8ce20ba0c3af75483051a87f71af97f45
DevOps <devops@rapidarchitect.com>
2024-06-22 09:57:50 -0500

    added docstrings

--- ol_mesop.py
+++ ol_mesop.py
@@ -1,17 +1,41 @@
 import mesop as me
 import mesop.labs as mel
-
 import ollama
+
+# default model to starling-lm for fast testing, change to a model you have installed in ollama
+model = 'starling-lm'
 
 @me.page(path="/chat")
 def chat():
-  mel.chat(transform)
+  """
+  This function is responsible for handling the "/chat" route and interacting with the OpenAI GPT model to generate responses based on user input.
+  :return: str
+  """
+  try:
+    response = mel.chat(transform)
+  except Exception as e:
+    print("An error occurred while trying to chat:", e)
 
 def transform(prompt: str, history: list[mel.ChatMessage]) -> str:
-  response = ollama.chat(model='llama3',messages=[
-  {
-    'role': 'user',
-    'content': f'{history} \n {prompt}',
-  },
-])
+  """
+  This function takes a prompt and a history of previous user inputs and generates a response using the OpenAI GPT model from the Ollama library.
+  
+  Args:
+    prompt (str): The user's input or question.
+    history (list[mel.ChatMessage]): A list of previous user inputs and corresponding responses.
+
+  Returns:
+    str: The generated response using GPT model.
+  """
+  try:
+    response = ollama.chat(model=model, messages=[
+      {
+        'role': 'user',
+        'content': f'{history} \n {prompt}',
+      },
+    ])
+  except Exception as e:
+    print("An error occurred while generating the response:", e)
+    return ""
+  
   return response['message']['content']
--- ol_stream_mesop.py
+++ ol_stream_mesop.py
@@ -8,9 +8,23 @@ model = 'llama3'
 
 @me.page(path="/chat")
 def chat():
+  """
+  This function is responsible for handling the "/chat" route and interacting with the OpenAI GPT model to generate responses based on user input.
+  :return: str
+  """
   mel.chat(transform)
 
 def transform(prompt: str, history: list[mel.ChatMessage]) -> str:
+  """
+  This function takes a prompt and a history of previous user inputs and generates a response using the OpenAI GPT model from the Ollama library.
+  
+  Args:
+    prompt (str): The user's input or question.
+    history (list[mel.ChatMessage]): A list of previous user inputs and corresponding responses.
+
+  Returns:
+    str: The generated response using GPT model.
+  """
   stream = ollama.chat(
     model = model,
     messages=[{'role': 'user', 'content': f'{history}\n{prompt}'}],
